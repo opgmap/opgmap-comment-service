75644b3345c2c390b6985421850e1c94d42fd4fe
Dinar Shagaliev <dinar.shagaliev2002@gmail.com>
2022-06-21 23:10:45 +0300

    added zipkin

--- src/test/java/ru/opgmap/opgmap_comment_service/OpgmapCommentServiceApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package ru.opgmap.opgmap_comment_service;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class OpgmapCommentServiceApplicationTests {
-
-}
